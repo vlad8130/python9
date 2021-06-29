65fcd9f89c215892b108a14497ad5480fcb2c743
unknown <vlisitskiy@gmail.com>
2021-06-29 20:29:17 +0300

    sfsdfjfoir

---
+++ Giveit.py
@@ -0,0 +1,14 @@
+# print ("Give it to me!")
+number = int(input())
+if (number >= 100):
+   print("Thanks man!")
+elif ((number > 10) and (number < 100)):
+   print ("OK :(")
+else:
+   print("WHAAAAAT???")
+
+if (number > 1000):
+	print ("!!!WOOOOWWW!!!")
+
+
+
