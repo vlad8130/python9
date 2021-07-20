19c8b72bd01e32b896c2310205dd5025ee0853f0
vlad8130 <vlisitskiy@gmail.com>
2021-07-20 09:03:00 +0300

    new

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+i  = [4, 5, 4, 5, 5]
+j  = [3, 3, 4, 2, 3 ]
+k  = [1, 2, 3, 2, 1]
+stud = ['john', 'james', 'kolya']
+
+a = sum (i)/ len(i)
+b = sum (j)/ len(j)
+c = sum (k)/ len(k)
+print(stud[0], "is the best!", a, end='\n' )
+print(stud[2], "is the worst!", c, end='\n' )
